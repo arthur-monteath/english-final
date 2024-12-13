045898117577ce935aa90c7c1017d90e762ee4b9
Arthur Monteath Trindade <thucatrindade1@gmail.com>
2024-12-13 08:23:42 -0300

    removed extra quotes

--- components/excerptAnalysis.tsx
+++ components/excerptAnalysis.tsx
@@ -8,7 +8,7 @@ interface ExcerptAnalysisProps {
       <div>
         <h3 className="text-2xl font-semibold mb-4">Excerpt</h3>
         <blockquote className="italic border-l-4 pl-4 mb-6">
-          "{excerpt}"
+          {excerpt}
         </blockquote>
         <h3 className="text-2xl font-semibold mb-4">Analysis</h3>
         <p className="">{analysis}</p>
